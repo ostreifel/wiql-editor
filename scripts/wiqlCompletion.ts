b30b170aef57e1168239ef7a4510fbbc0177cda3
Otto Streifel <ottost@microsoft.com>
2016-11-29 22:32:47 -0800

    suggest variable names

--- scripts/wiqlCompletion.ts
+++ scripts/wiqlCompletion.ts
@@ -1,13 +1,14 @@
-import {WorkItemField} from 'TFS/WorkItemTracking/Contracts';
-import {tokenize, opMap, symbolMap} from './wiqlTokenizer';
+import { WorkItemField } from 'TFS/WorkItemTracking/Contracts';
+import { tokenize, opMap, symbolMap } from './wiqlTokenizer';
 import * as Symbols from './wiqlSymbols';
-import {parse, ParseError} from './wiqlParser';
+import { parse, ParseError } from './wiqlParser';
+import { validVariableNames } from './wiqlDefinition';
 
 // These symbols are buggy when suggested
 // brackets are not paired, rbrackets and commas suggested when its a syntax error to do so 
 const doNotSuggest = ['(', ')', ',', '[', ']'];
 
-const symbolSuggestionMap: {[symbolName: string]: monaco.languages.CompletionItem} = {};
+const symbolSuggestionMap: { [symbolName: string]: monaco.languages.CompletionItem } = {};
 for (let map of [opMap, symbolMap]) {
 	for (let label in map) {
 		if (doNotSuggest.indexOf(label) < 0) {
@@ -23,14 +24,22 @@ export const getCompletionProvider: (fields: WorkItemField[]) => monaco.language
 	const fieldLabels = fields.filter((f) => f.name.indexOf(' ') < 0)
 		.map((f) => f.name)
 		.concat(fields.map((f) => f.referenceName))
-	const fieldSuggestions = fieldLabels.map((label) => { return { 
-		label: label, 
-		kind: monaco.languages.CompletionItemKind.Variable
-	}});
+	const fieldSuggestions = fieldLabels.map((label) => {
+		return {
+			label: label,
+			kind: monaco.languages.CompletionItemKind.Variable
+		};
+	});
+	const variableSuggestions = validVariableNames.map((v) => {
+		return {
+			label: v,
+			kind: monaco.languages.CompletionItemKind.Variable
+		};
+	})
 	return {
 		provideCompletionItems: (model, position, token) => {
 			const lines = model.getLinesContent().slice(0, position.lineNumber);
-			if (lines.length > 0 ) {
+			if (lines.length > 0) {
 				lines[lines.length - 1] = lines[lines.length - 1].substr(0, position.column - 1);
 			}
 			const parseResult = parse(lines);
@@ -44,12 +53,14 @@ export const getCompletionProvider: (fields: WorkItemField[]) => monaco.language
 			if (!(parseNext instanceof ParseError) || parseNext.remainingTokens > 2) {
 				// valid query, can't suggest
 				return [];
-			} else if ((prevToken = parseNext.parsedTokens[parseNext.parsedTokens.length -1]) instanceof Symbols.Identifier
-				&& position.column - 1 === prevToken.endColumn
-				&& fieldLabels.indexOf(prevToken.value) < 0) {
+			} else if ((prevToken = parseNext.parsedTokens[parseNext.parsedTokens.length - 1]) instanceof Symbols.Identifier
+				&& position.column - 1 === prevToken.endColumn) {
 				// In process of typing field name
 				// (parser just consumes this becuase it doesn't know which fields are valid)
 				return fieldSuggestions;
+			} else if ((prevToken = parseNext.parsedTokens[parseNext.parsedTokens.length - 1]) instanceof Symbols.Variable
+				&& position.column - 1 === prevToken.endColumn) {
+				return variableSuggestions;
 			} else {
 				const suggestions: monaco.languages.CompletionItem[] = [];
 				for (let token of parseNext.expectedTokens) {
@@ -59,6 +70,9 @@ export const getCompletionProvider: (fields: WorkItemField[]) => monaco.language
 				}
 				if (parseNext.expectedTokens.indexOf(Symbols.getSymbolName(Symbols.Identifier)) >= 0) {
 					suggestions.push(...fieldSuggestions);
+				}
+				if (parseNext.expectedTokens.indexOf(Symbols.getSymbolName(Symbols.Variable)) >= 0) {
+					suggestions.push(...variableSuggestions);
 				}
 				return suggestions;
 			}
