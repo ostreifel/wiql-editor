c8cc9ecf75c218a5b2811f898daabd3e6e636a21
Otto Streifel <ottost@microsoft.com>
2016-12-08 13:29:34 -0800

    v1.0.7 updates readme, syntax highlighting

--- details.md
+++ details.md
@@ -11,6 +11,12 @@ Save changes to the query when finished.
 
  ![Image of wiql dialog](img/dialog.png)
 
+ Currently only flat queries are supported.
+
 # Change Log
+#### v1.0.5
+Changes syntax highlighting.
+
+Disclaimer about query types that are supported.
 #### v1.0.1
 Initial Release
--- scripts/wiqlErrorCheckers/SyntaxErrorChecker.ts
+++ scripts/wiqlErrorCheckers/SyntaxErrorChecker.ts
@@ -11,22 +11,24 @@ enum ComparisonType {
 
 export class SyntaxErrorChecker {
     public check(parseResult: IParseResults): monaco.editor.IModelDeltaDecoration[] {
-    if (!(parseResult instanceof ParseError)) {
-        return [];
-    }
-    let errorToken: Symbols.Token;
-    let hoverMessage: string;
-    if (parseResult.errorToken instanceof Symbols.EOF) {
-        errorToken = parseResult.errorToken.prev;
-        hoverMessage = parseResult.expectedTokens.length === 1 ?
-            `Should be followed by ${parseResult.expectedTokens[0]}` :
-            `Should be followed by one of {${parseResult.expectedTokens.join(', ')}}`;
-    } else {
-        errorToken = parseResult.errorToken;
-        hoverMessage = parseResult.expectedTokens.length === 1 ?
-            `Expected ${parseResult.expectedTokens[0]}` :
-            `Expected one of {${parseResult.expectedTokens.join(', ')}}`;
-    }
-    return [toDecoration(errorToken, hoverMessage)];
+        if (!(parseResult instanceof ParseError)) {
+            return [];
+        }
+        let errorToken: Symbols.Token;
+        let hoverMessage: string;
+        if (parseResult.errorToken instanceof Symbols.EOF) {
+            errorToken = parseResult.errorToken.prev;
+            hoverMessage = parseResult.expectedTokens.length === 1 ?
+                `Should be followed by ${parseResult.expectedTokens[0]}` :
+                `Should be followed by one of {${parseResult.expectedTokens.join(', ')}}`;
+        } else {
+            errorToken = parseResult.errorToken;
+            hoverMessage = parseResult.expectedTokens.length === 1 ?
+                `Expected ${parseResult.expectedTokens[0]}` :
+                `Expected one of {${parseResult.expectedTokens.join(', ')}}`;
+        }
+        const decoration = toDecoration(errorToken, hoverMessage);
+        decoration.range = decoration.range.setEndPosition(Infinity, Infinity);
+        return [decoration];
     }
 }
