7213021b7d2d147d7e84ead6d83978afa822f243
Otto Streifel <ottost@microsoft.com>
2017-02-04 19:57:44 -0800

    v1.3.12 variable hovers

--- extension/scripts/wiqlHoverProvider.ts
+++ extension/scripts/wiqlHoverProvider.ts
@@ -2,6 +2,7 @@ import { WorkItemField, FieldType } from "TFS/WorkItemTracking/Contracts";
 import { symbolsOfType } from "./wiqlErrorCheckers/errorCheckUtils";
 import { parse } from "./compiler/wiqlParser";
 import * as Symbols from "./compiler/wiqlSymbols";
+import { definedVariables } from "./wiqlDefinition";
 
 function filterByPosition(tokens: Symbols.Token[], position: monaco.Position) {
     return tokens.filter(token =>
@@ -18,23 +19,32 @@ export function getHoverProvider(fields: WorkItemField[]): monaco.languages.Hove
 
             const parseResult = parse(lines);
             const ids = symbolsOfType<Symbols.Identifier>(parseResult, Symbols.Identifier);
-            const idsAtPos = filterByPosition(ids, position);
+            const [id] = filterByPosition(ids, position);
 
             const hovers: monaco.MarkedString[] = [];
             let range: monaco.IRange = null as any;
-            if (idsAtPos.length > 0) {
-                const id = idsAtPos[0];
+            if (id) {
                 const matchedFields = fields.filter(f =>
                     f.name.toLocaleLowerCase() === id.text.toLocaleLowerCase() ||
                     f.referenceName.toLocaleLowerCase() === id.text.toLocaleLowerCase()
                 );
                 if (matchedFields.length > 0) {
                     hovers.push(FieldType[matchedFields[0].type]);
-                    range = new monaco.Range(id.line + 1, id.startColumn, id.line + 1, id.endColumn + 1);
+                    range = new monaco.Range(id.line + 1, id.startColumn + 1, id.line + 1, id.endColumn + 1);
+                }
+            } else {
+                const vars = symbolsOfType<Symbols.Identifier>(parseResult, Symbols.Variable);
+                const [variable] = filterByPosition(vars, position);
+                if (variable) {
+                    const matchedVariable = variable.text.toLocaleLowerCase() in definedVariables;
+                    if (matchedVariable) {
+                        hovers.push(FieldType[definedVariables[variable.text.toLocaleLowerCase()]]);
+                        range = new monaco.Range(variable.line + 1, variable.startColumn + 1, variable.line + 1, variable.endColumn + 1);
+                    }
                 }
             }
 
-            return {contents: hovers, range};
+            return { contents: hovers, range };
         }
     };
 }
