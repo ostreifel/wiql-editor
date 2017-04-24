71c971d6e314948ee55b28483cc8de55e7cf9118
Otto Streifel <ottost@microsoft.com>
2017-04-24 15:16:45 -0700

    v1.6.36 detect api version

--- extension/scripts/cachedData/fields.ts
+++ extension/scripts/cachedData/fields.ts
@@ -2,13 +2,17 @@ import { WorkItemField, GetFieldsExpand } from "TFS/WorkItemTracking/Contracts";
 import { getClient as getWitClient } from "TFS/WorkItemTracking/RestClient";
 import { CachedValue } from "./CachedValue";
 
-export const fields: CachedValue<WorkItemField[]> = new CachedValue(getFields);
+export const fields: CachedValue<WorkItemField[]> = new CachedValue<WorkItemField[]>(getFields);
 
 function getFields(): IPromise<WorkItemField[]> {
     const client = getWitClient();
-    /** The type definition for fields in the sdk is wrong, this is the actual type */
+    /** The type definition for fields in the sdk is wrong, this is the actual type if the server is at the latest version */
     const getFields: (projectId?: string, expand?: GetFieldsExpand) => IPromise<WorkItemField[]> = <any>client.getFields.bind(client);
-    return getFields(undefined, GetFieldsExpand.ExtensionFields);
+    if (getFields.length === 2) {
+        return getFields(undefined, GetFieldsExpand.ExtensionFields);
+    }
+    // Older server -- fallback
+    return this.getWitClient().getFields(GetFieldsExpand.ExtensionFields)
 }
 
 export function getField(refOrName: string, fields: WorkItemField[]): WorkItemField | undefined {
